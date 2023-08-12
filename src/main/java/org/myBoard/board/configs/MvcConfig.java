cd161e7db5a00ad183eb59ce697bf15b09c13fe2
BlueOceanDolphin <ehfrhfo313@naver.com>
2023-08-13 07:27:29 +0900

    휴대전화번호 검증 추가

--- src/main/java/org/myBoard/board/configs/MvcConfig.java
+++ src/main/java/org/myBoard/board/configs/MvcConfig.java
@@ -1,10 +1,15 @@
 package org.myBoard.board.configs;
 
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.myBoard.board.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -37,5 +42,19 @@ public class MvcConfig implements WebMvcConfigurer { // 설정
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(siteConfigInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons", "messages.validations", "messages.errors");
+
+        return ms;
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() { // GET. POST외에 DELETE, PATCH, PUT ... 추가할 수 있는 설정
+        return  new HiddenHttpMethodFilter();
     }
 }
--- src/main/java/org/myBoard/board/configs/SecurityConfig.java
+++ src/main/java/org/myBoard/board/configs/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.authentication.PasswordEncoderParser;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +19,10 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return w -> w.ignoring().requestMatchers("/css/**", "/js/**", "/images/**", "/errors/**"); // 무력화
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
