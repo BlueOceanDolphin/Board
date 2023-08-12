b99f1bbcab169ef7e397efd766edd0c056926a92
BlueOceanDolphin <ehfrhfo313@naver.com>
2023-08-13 01:07:55 +0900

    스프링 시큐리티 기본설정, 정적 경로 설정, 임시 메인 페이지 추가

---
+++ src/main/java/org/myBoard/board/configs/MvcConfig.java
@@ -0,0 +1,27 @@
+package org.myBoard.board.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer { // 설정
+
+    @Value("${file.upload.path}") // lombok이 아니라 springframework로 해야함
+    private String fileUploadPath;
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("main/index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:///" + fileUploadPath);
+    }
+}
