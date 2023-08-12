ec211d197e761a1a62021a522cc3d40af679c689
BlueOceanDolphin <ehfrhfo313@naver.com>
2023-08-13 02:03:10 +0900

    템플릿 레이아웃 프론트 완료

--- src/main/java/org/myBoard/board/configs/MvcConfig.java
+++ src/main/java/org/myBoard/board/configs/MvcConfig.java
@@ -1,17 +1,23 @@
 package org.myBoard.board.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.myBoard.board.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer { // 설정
 
     @Value("${file.upload.path}") // lombok이 아니라 springframework로 해야함
     private String fileUploadPath;
 
+    // 사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor; // 인터셉터 주입
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -23,5 +29,11 @@ public class MvcConfig implements WebMvcConfigurer { // 설정
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///" + fileUploadPath);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
     }
 }
