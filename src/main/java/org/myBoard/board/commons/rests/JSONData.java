6a1f53280a9fff68c3a6ea89c602603a2a0c400d
BlueOceanDolphin <ehfrhfo313@naver.com>
2023-08-15 19:07:20 +0900

    공통 예외 처리 완료

--- src/main/java/org/myBoard/board/commons/rests/JSONData.java
+++ src/main/java/org/myBoard/board/commons/rests/JSONData.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @Data @Builder
 @NoArgsConstructor @AllArgsConstructor
 public class JSONData<T> {
-    private boolean Success;
+    private boolean success;
     private HttpStatus status = HttpStatus.OK; // 200코드가 가장 많으므로 200
     private String message;
     private T data;
